f0dc49e0e311a352d91b1ac5945c364fb9a0f8e9
Brolaf <121128513+Ronkad02@users.noreply.github.com>
2024-12-07 22:00:19 +0100

    Update and rename buzzer1_5_10.ino to buzzer1_6_10.ino
    shortend RAM usage

--- buzzer1_6_10.ino
+++ buzzer1_6_10.ino
@@ -1,28 +1,28 @@
-const int butStart = 12;
-const int butLinksDown = 10;
-const int butRechtsUp = 11;
-const int butPlayPause = 9;
-const int butBreak = 8;
-const int ledPlayPause = 3;
-const int led3 = 7;
-const int led2 = 6;
-const int led1 = 5;
-const int led0 = 4;
-const int buzzer1 =2;
+const unsigned char butStart = 12;
+const unsigned char butLinksDown = 10;
+const unsigned char butRechtsUp = 11;
+const unsigned char butPlayPause = 9;
+const unsigned char butBreak = 8;
+const unsigned char ledPlayPause = 3;
+const unsigned char led3 = 7;
+const unsigned char led2 = 6;
+const unsigned char led1 = 5;
+const unsigned char led0 = 4;
+const unsigned char buzzer1 =2;
 
 
-int butStatStart = 0;
-int butStatLinksDown = 0;
-int butStatRechtsUp = 0;
-int butStatPlayPause = 0;
-int butStatBreak = 0;
-int ledLichtZaehler = 0;
+bool butStatStart = 0;
+bool butStatLinksDown = 0;
+bool butStatRechtsUp = 0;
+bool butStatPlayPause = 0;
+bool butStatBreak = 0;
+short ledLichtZaehler = 0;
 
-int maxLieder = 11;  //max 15; min 1
+unsigned char maxLieder = 12;  //max 15; min 1
 
 
 //Liederarrays
-int melody1[] = {   //Happy Birthday 2 Strophen
+short melody1[] = {   //Happy Birthday 2 Strophen
   238, 238, 267, 238, 317, 297,
   238, 238, 267, 238, 356, 317,
   238, 238, 475, 396, 317, 297, 267,
@@ -32,7 +32,7 @@ int melody1[] = {   //Happy Birthday 2 Strophen
   238, 238, 475, 396, 317, 297, 267,
   419, 419, 396, 317, 356, 317
 };
-int noteDurations1[] = {
+short noteDurations1[] = {
   200, 200, 450, 450, 450, 950,
   200, 200, 450, 450, 450, 950,
   200, 200, 450, 450, 450, 450, 950,
@@ -42,7 +42,7 @@ int noteDurations1[] = {
   200, 200, 450, 450, 450, 450, 950,
   200, 200, 450, 450, 450, 950
 };
-int pausDurations1[] = {
+unsigned char pausDurations1[] = {
   100,
   50, 50, 50, 50, 50, 50,
   50, 50, 50, 50, 50, 50,
@@ -55,26 +55,20 @@ int pausDurations1[] = {
 };
 
 
-int melody2[] = {   //Wallermann
-  392, 261, 261, 261, 330,
-  392, 392, 392, 392, 440,
-  349, 349, 349, 349, 440,
-  523, 523, 392, 392, 392,
-  261, 261, 261, 330, 392,
-  392, 392, 392, 392, 349,
-  330, 330, 293, 261
+short melody2[] = {  //Leise rieselt der Schnee
+  220, 220, 196, 220, 196, 174, 174, 174, 146, 174, 164, 146, 130, 130, 196, 184, 196, 246, 220, 196,
+  174, 174, 196, 146, 146, 164, 146, 164, 174, 174
 };
-int noteDurations2[] = {
-  450, 450, 450, 450, 450,
-  450, 450, 700, 200, 450,
-  200, 200, 450, 200, 200,
-  200, 200, 450, 700, 200,
-  450, 450, 450, 200, 450,
-  450, 700, 200, 450, 450,
-  200, 200, 450, 950
+short noteDurations2[] = { 
+  950, 450, 700, 200, 450, 1450, 1450, 950, 450, 700, 200, 450, 1450, 1450, 700, 200, 450, 450,
+  450, 450, 1450, 1450, 700, 200, 450, 450, 450, 450, 1450, 1450
+};
+unsigned char pausDurations2[] = { 50,
+  50, 50, 50, 50, 50, 0, 50, 50, 50, 50, 50, 50, 0, 50, 50, 50, 50, 50,
+  50, 50, 0, 50, 50, 50, 50, 50, 50, 50, 0, 50
 };
 
-int melody3[] = {   // Super Mario Brostheme  //tempo 200
+short melody3[] = {   // Super Mario Brostheme  //tempo 200
     /*More songs available at https://github.com/robsoncouto/arduino-songs       
                                               Robson Couto, 2019*/
   // Score available at https://musescore.com/user/2123/scores/2145
@@ -97,17 +91,17 @@ int melody3[] = {   // Super Mario Brostheme  //tempo 200
 };
 
 
-int melody4[] = {   //StarWars
+short melody4[] = {   //StarWars
   196, 196, 196, 155, 233, 196, 155, 233, 196, 
   293, 293, 293, 311, 233, 185, 155, 233, 196
 };
-int noteDurations4[] = {
+short noteDurations4[] = {
   500, 500, 500, 350, 150, 500, 350, 150, 1000,
   500, 500, 500, 350, 150, 500, 350, 150, 1000
 };
 
 
-int melody5[] = { // Game of Thrones  //tempo 85
+short melody5[] = { // Game of Thrones  //tempo 85
   // Score available at https://musescore.com/user/8407786/scores/2156716
   392,8, 262,8, 311,16, 349,16, 392,8, 262,8, 311,16, 349,16, //1
   392,8, 262,8, 311,16, 349,16, 392,8, 262,8, 311,16, 349,16,
@@ -120,83 +114,82 @@ int melody5[] = { // Game of Thrones  //tempo 85
   311,16, 349,16, 392,4, 262,4, 311,16, 349,16, 294,-2,
 };
 
-int melody6[] = {//a million Dream (the greates Showman)
+short melody6[] = {//a million Dream (the greates Showman)
   246, 246, 261, 294, 294, 261, 246, 261, 294, 294, 261, 246, 261, 294, 294, 261, 
   330, 330, 261, 246, 246, 196, 220, 246, 294, 330
 };
-int noteDurations6[] = {
+short noteDurations6[] = {
   200, 450, 200, 225, 725, 200, 450, 200, 225, 725, 200, 450, 200, 225, 475, 200,
   225, 475, 200, 225, 1475, 200, 200, 950, 950, 950
 };
-int pausDurations6[] = {
+unsigned char pausDurations6[] = {
   50, 50, 50, 0, 50, 50, 50, 50, 0, 50, 50, 50, 50, 0, 50, 50, 
   0, 50, 50, 0, 50, 50, 50, 50, 50, 50
 };
 
 
-int melody7[] = {//Für Elise (Beethoven)
+short melody7[] = {//Für Elise (Beethoven)
   330, 311, 330, 311, 330, 246, 294, 261, 220, 130, 330, 220, 246, 330, 208, 246, 261, 330, 311, 
   330, 311, 330, 246, 294, 261, 220, 130, 330, 220, 246, 261, 246, 220
 };
-int noteDurations7[] = {
+short noteDurations7[] = {
   171, 171, 171, 171, 171, 171, 171, 171, 632, 171, 171, 171, 491, 171, 171, 171, 391, 
   171, 171, 171, 171, 171, 171, 171, 171, 632, 171, 171, 171, 491, 171, 171, 391
 };
-int pausDurations7[] = { 100,
+short pausDurations7[] = { 100,
   50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 150, 50, 50, 50, 491, 
   50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 150, 50, 50, 50
 };
 
 
-int melody8[] = { //Hes the Pirat (Fluch der Karribik)
+short melody8[] = { //Hes the Pirat (Fluch der Karribik)
   220, 261, 293, 293, 293, 329, 349, 349, 349, 392, 329, 329, 293, 261, 261, 293, 
   220, 261, 293, 293, 293, 329, 349, 349, 349, 392, 329, 329, 293, 261, 293
 };
-int noteDurations8[] = { 
+short noteDurations8[] = { 
   200, 200, 450, 450, 200, 200, 450, 450, 200, 200, 450, 450, 
   200, 200, 200, 450, 200, 200, 450, 450, 200, 200, 450, 450, 
   200, 200, 450, 450, 200, 200, 700
 };
-int pausDurations8[] = {
+unsigned char pausDurations8[] = {
   100, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 
   50, 50, 50, 250, 50, 50, 50, 50, 50, 50, 50, 50, 
   50, 50, 50, 50, 50, 50, 50
 };
 
 
-int melody9[] = {//Wham Last Christmas
+short melody9[] = {//Wham Last Christmas
   329, 329, 294, 220, 329, 329, 370, 294, 247, 247,
   329, 329, 370, 294, 247, 277, 294, 277, 247, 370,
   329, 247, 370, 392, 370, 329, 247, 277, 294, 277,
   277, 294, 277, 220
 };
-int noteDurations9[] = {
+short noteDurations9[] = {
   495, 360, 360, 223, 223, 223, 223, 360, 223, 223,
   223, 223, 360, 360, 223, 223, 223, 223, 768, 768,
   495, 223, 223, 223, 223, 495, 223, 223, 223, 223,
   300, 300, 300, 1000
 };
 
-int melody10[] = { //Theme from Mission: Impossible by Lalo Schifrin
+short melody10[] = { //Theme from Mission: Impossible by Lalo Schifrin
   220, 220, 262, 294, 220, 220, 196, 208, 220, 220, 262, 294, 220, 220, 196, 208, 
   262, 220, 164, 262, 220, 312, 262, 220, 146, 130, 146,
   220, 220, 262, 294, 220, 220, 196, 208, 262, 220, 415, 262, 220, 415,
   262, 220, 370, 349, 330,
   220, 262, 294, 220, 220, 196, 208, 220, 220, 262, 294, 220, 220, 196, 208
 };
-int noteDurations10[] = { 
+short noteDurations10[] = { 
   303, 480, 303, 303, 303, 480, 303, 303, 303, 480, 303, 303, 303, 480, 303, 303,
   126, 126, 1362, 126, 126, 1362, 126, 126, 1362, 126, 1488, 303, 480, 303, 303,
   303, 480, 303, 303, 126, 126, 1362, 126, 126, 1362, 126, 126, 1362, 126, 126,
   303, 303, 303, 303, 480, 303, 303, 303, 480, 303, 303, 303, 480, 303, 303
 };
-int pausDurations10[] = { 100,
+short pausDurations10[] = { 100,
   353, 50, 50, 50, 353, 50, 50, 50, 353, 50, 50, 50, 353, 50, 50, 50,
   50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 353, 50, 50, 50, 353,
   50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 10, 50, 50, 50,
   50, 353, 50, 50, 50, 353, 50, 50, 50, 353, 50, 50, 50
 };
-
 
 
 void setup(){
@@ -214,7 +207,7 @@ pinMode(buzzer1, OUTPUT);
 }
 
 bool pauseLoop () {
-  int helligkeit=0;
+  unsigned char helligkeit=0;
   digitalWrite(ledPlayPause, HIGH);
     while (true) 
     {
@@ -248,7 +241,7 @@ bool pauseLoop () {
   digitalWrite(ledPlayPause, LOW);
 }
 
-void ledlichter(int a) {
+void ledlichter(short a) {
   if(a>maxLieder)   //setzt a
   {
     a = 0;
@@ -266,11 +259,11 @@ void ledlichter(int a) {
 }
 
 
-void ar1song(int melody[], int tempo, int arraySize) {
-  int notes = arraySize / 2;
+void ar1song(short melody[], unsigned char tempo, unsigned char arraySize) {
+  unsigned char notes = arraySize / 2;
   int wholenote = (60000 * 4) / tempo;
-  int divider = 0, noteDuration = 0;
-  for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
+  short divider = 0, noteDuration = 0;
+  for (unsigned char thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
     butStatPlayPause = !digitalRead(butPlayPause);
     butStatBreak = !digitalRead(butBreak);
     if (butStatBreak)
@@ -281,7 +274,7 @@ void ar1song(int melody[], int tempo, int arraySize) {
     {
       if (butStatPlayPause)
       {
-        int ret=0;
+        bool ret=0;
         ret = pauseLoop();
         if(ret)
         {
@@ -305,9 +298,9 @@ void ar1song(int melody[], int tempo, int arraySize) {
   }
 }
 
-void ar2song(int melody[], int noteDurations[], int arraySize) { 
+void ar2song(short melody[], short noteDurations[], unsigned char arraySize) { 
   delay(50);
-  for (int curentNote = 0; curentNote < arraySize; curentNote++)
+  for (unsigned char curentNote = 0; curentNote < arraySize; curentNote++)
   {
     butStatPlayPause = !digitalRead(butPlayPause);   //nur Pause
     butStatBreak = !digitalRead(butBreak);
@@ -320,7 +313,7 @@ void ar2song(int melody[], int noteDurations[], int arraySize) {
         {
          if (butStatPlayPause) //macht pause, durch while loop, geht erst weigter nach erneuten button drücken mit delay
           {
-            int ret=0;
+            bool ret=0;
             ret = pauseLoop();
             if(ret)
             {
@@ -329,7 +322,7 @@ void ar2song(int melody[], int noteDurations[], int arraySize) {
           } 
             else  //eine Note im Array abspielen und eine pause machen nach der Note (Pause durch array oder dauerhafte zeit festlegen)
             {
-              int noteDuration = noteDurations[curentNote];
+              short noteDuration = noteDurations[curentNote];
               tone(buzzer1, melody[curentNote]);
              delay(noteDuration);
 
@@ -339,10 +332,9 @@ void ar2song(int melody[], int noteDurations[], int arraySize) {
   }
 }
 
-
-void ar3song(int melody[], int noteDurations[], int pausDurations[], int arraySize) { 
-  for (int curentNote = 0; curentNote < arraySize; curentNote++) {
-    int pauseDuration = pausDurations[curentNote];
+void ar3song(short melody[], short noteDurations[], short pausDurations[], unsigned char arraySize) { 
+  for (unsigned char curentNote = 0; curentNote < arraySize; curentNote++) {
+    short pauseDuration = pausDurations[curentNote];
     bool butStatPlayPause = !digitalRead(butPlayPause); // Read the play/pause button
     bool butStatBreak = !digitalRead(butBreak);
 
@@ -352,13 +344,13 @@ void ar3song(int melody[], int noteDurations[], int pausDurations[], int arraySi
       break; // Exit the loop if the break button is pressed
     } else {
       if (butStatPlayPause) { // Pause functionality
-        int ret = 0;
+        bool ret = 0;
         ret = pauseLoop(); // Pause until button is pressed again
         if (ret) {
           break;
         }
       } else { // Play the current note and pause
-        int noteDuration = noteDurations[curentNote];
+        short noteDuration = noteDurations[curentNote];
         tone(buzzer1, melody[curentNote]);
         delay(noteDuration);
         noTone(buzzer1);
@@ -367,69 +359,89 @@ void ar3song(int melody[], int noteDurations[], int pausDurations[], int arraySi
   }
 }
 
-void playSong (int a) {
+void ar3song(short melody[], short noteDurations[], unsigned char pausDurations[], unsigned char arraySize) { 
+  for (short curentNote = 0; curentNote < arraySize; curentNote++) {
+    unsigned char pauseDuration = pausDurations[curentNote];
+    butStatPlayPause = !digitalRead(butPlayPause); // Read the play/pause button
+    butStatBreak = !digitalRead(butBreak);
+
+    delay(pauseDuration);
+    
+    if (butStatBreak) {
+      break; // Exit the loop if the break button is pressed
+    } else {
+      if (butStatPlayPause) { // Pause functionality
+        bool ret = 0;
+        ret = pauseLoop(); // Pause until button is pressed again
+        if (ret) {
+          break;
+        }
+      } else { // Play the current note and pause
+        short noteDuration = noteDurations[curentNote];
+        tone(buzzer1, melody[curentNote]);
+        delay(noteDuration);
+        noTone(buzzer1);
+      }
+    }
+  }
+}
+
+void playSong (unsigned char a) {
 switch (a) {
 case 1: {
-  unsigned char arraySize = sizeof(melody1) / sizeof(melody1[0]);
-  ar3song(melody1, noteDurations1, pausDurations1, arraySize);
+  ar3song(melody1, noteDurations1, pausDurations1, sizeof(melody1) / sizeof(melody1[0]));
   break;
 }
 case 2: {
-  unsigned char arraySize = sizeof(melody2) / sizeof(melody2[0]);
-  ar2song(melody2, noteDurations2, arraySize);
+  ar3song(melody2, noteDurations2, pausDurations2, sizeof(melody2) / sizeof(melody2[0]));
   break;
 }
 
 case 3: {
-  unsigned char arraySize = sizeof(melody3) / sizeof(melody3[0]);
-  ar1song(melody3, 200, arraySize);
+  const unsigned char tempo = 200;
+  ar1song(melody3, tempo, sizeof(melody3) / sizeof(melody3[0]));
   break;
 }
 
 case 4: {
-  unsigned char arraySize = sizeof(melody4) / sizeof(melody4[0]);
-  ar2song(melody4, noteDurations4, arraySize);
+  ar2song(melody4, noteDurations4, sizeof(melody4) / sizeof(melody4[0]));
   break;
 }
 
 case 5: {
-  unsigned char arraySize = sizeof(melody5) / sizeof(melody5[0]);
-  ar1song(melody5, 85, arraySize);
+  const unsigned char tempo = 85;
+  ar1song(melody5, tempo, sizeof(melody5) / sizeof(melody5[0]));
   break;
 }
 
 case 6: {
-  unsigned char arraySize = sizeof(melody6) / sizeof(melody6[0]);
-  ar3song(melody6, noteDurations6, pausDurations6, arraySize);
+  ar3song(melody6, noteDurations6, pausDurations6, sizeof(melody6) / sizeof(melody6[0]));
   break;
 }
 
 case 7: {
-  unsigned char arraySize = sizeof(melody7) / sizeof(melody7[0]);
-  ar3song(melody7, noteDurations7, pausDurations7, arraySize);
+  ar3song(melody7, noteDurations7, pausDurations7, sizeof(melody7) / sizeof(melody7[0]));
   break;
 }
 
 case 8: {
-  unsigned char arraySize = sizeof(melody8) / sizeof(melody8[0]);
-  ar3song(melody8, noteDurations8, pausDurations8, arraySize);
+  ar3song(melody8, noteDurations8, pausDurations8, sizeof(melody8) / sizeof(melody8[0]));
   break;
 }
 
 case 9: {
-  unsigned char arraySize = sizeof(melody9) / sizeof(melody9[0]);
-  ar2song(melody9, noteDurations9, arraySize);
+  ar2song(melody9, noteDurations9, sizeof(melody9) / sizeof(melody9[0]));
   break;
 }
 
-case 10:
-  unsigned char arraySize = sizeof(melody10) / sizeof(melody10[0]);
-  ar3song(melody10, noteDurations10, pausDurations8, arraySize);
+case 10:{
+  ar3song(melody10, noteDurations10, pausDurations10, sizeof(melody10) / sizeof(melody10[0]));
   break;
+}
 /*
-case 11:
-
-break;
+case 11:{
+  break;
+}
 case 12:
 
 break;
@@ -452,12 +464,12 @@ void loop() {
   butStatLinksDown = !digitalRead(butLinksDown);
   butStatRechtsUp = !digitalRead(butRechtsUp);
   butStatPlayPause = !digitalRead(butPlayPause);
-  static int b = 1;
+  static bool b = 0;
   if (butStatPlayPause)
   {
     if (b==1)
     {
-    b=2;
+    b=0;
     }else {
       b=1;
       }
@@ -465,13 +477,13 @@ void loop() {
   
   if (butStatLinksDown)
   {
-    ledLichtZaehler = ledLichtZaehler + b;
+    ledLichtZaehler = ledLichtZaehler + b + 1;
     ledlichter(ledLichtZaehler);
   }
 
   if (butStatRechtsUp)
   {
-    ledLichtZaehler = ledLichtZaehler - b;
+    ledLichtZaehler = ledLichtZaehler - b - 1;
     ledlichter(ledLichtZaehler);
   }
 
